71406d8a68c0c4d7b3bc996c9088a20dc3f416d1
Your Github User Name <david.lav.williams@gmail.com>
2020-10-01 10:05:58 -0500

    A website about my local congressmen.

---
+++ main.js
@@ -0,0 +1,10 @@
+// I'm just trying to make things interactive. Please ignore.
+
+
+// console.log("hi")
+// $("img").mouseover(function(){
+//     this.style.width = "50%"
+// })
+// $("li").mouseleave(function(){
+//     this.style.font-size = "20px"
+//   })
